34266bb1a2f8b8c7499e25b59877c3c82e09a947
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-04-06 21:45:58 +0300

    Update Query3.sql

--- Query3.sql
+++ Query3.sql
@@ -1,3 +1,39 @@
+-- Code 1
+with 
+month_data as(
+  SELECT
+    "Month" as time_type,
+    format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
+    trafficSource.source AS source,
+    SUM(p.productRevenue)/1000000 AS revenue
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
+    unnest(hits) hits,
+    unnest(product) p
+  WHERE p.productRevenue is not null
+  GROUP BY 1,2,3
+  order by revenue DESC
+),
+
+week_data as(
+  SELECT
+    "Week" as time_type,
+    format_date("%Y%W", parse_date("%Y%m%d", date)) as week,
+    trafficSource.source AS source,
+    SUM(p.productRevenue)/1000000 AS revenue
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`,
+    unnest(hits) hits,
+    unnest(product) p
+  WHERE p.productRevenue is not null
+  GROUP BY 1,2,3
+  order by revenue DESC
+)
+
+select * from month_data
+union all
+select * from week_data;
+order by time_type
+
+-- Code 2
 WITH raw_data AS
 (
 SELECT
@@ -30,7 +66,6 @@ FROM raw_data
 GROUP BY source, type_time, time
 
 ORDER BY revenue DESC;
-
 
 -- Revenue by traffic source by week, by month in June 2017
 -- Hint 1: separate month and week data then union all.

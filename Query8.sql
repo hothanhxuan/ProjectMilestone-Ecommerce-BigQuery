1c923001a6a6315e4790cb900ab43df531696296
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-04-06 21:57:15 +0300

    Update Query8.sql

--- Query8.sql
+++ Query8.sql
@@ -1,54 +1,75 @@
-SELECT 
-      month
-      ,num_product_view
-      ,num_addtocart
-      ,num_purchase
-      ,ROUND((num_addtocart / num_product_view) * 100.0, 2) AS add_to_cart_rate
-      ,ROUND((num_purchase / num_product_view) * 100.0, 2) AS purchase_rate
-FROM
-(
-SELECT  
-      FORMAT_DATE('%Y%m', PARSE_DATE('%Y%m%d', date)) AS month
-      ,COUNT(eCommerceAction.action_type) AS num_product_view
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits,
-UNNEST (hits.product) product 
-WHERE _TABLE_SUFFIX BETWEEN '0101' AND '0331'
-  AND  eCommerceAction.action_type = '2'
-GROUP BY month
-ORDER BY month
-) 
-LEFT JOIN 
-(
-SELECT  
-      FORMAT_DATE('%Y%m', PARSE_DATE('%Y%m%d', date)) AS month
-      ,COUNT(eCommerceAction.action_type) AS num_addtocart
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits,
-UNNEST (hits.product) product 
-WHERE _TABLE_SUFFIX BETWEEN '0101' AND '0331'
-  AND  eCommerceAction.action_type = '3'
-GROUP BY month
-ORDER BY month
-)
-USING (month)
-LEFT JOIN 
-(
-SELECT  
-      FORMAT_DATE('%Y%m', PARSE_DATE('%Y%m%d', date)) AS month
-      ,COUNT(eCommerceAction.action_type) AS num_purchase 
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits,
-UNNEST (hits.product) product 
-WHERE _TABLE_SUFFIX BETWEEN '0101' AND '0331'
-  AND eCommerceAction.action_type = '6'
-  AND product.productRevenue IS NOT NULL 
-GROUP BY month
-ORDER BY month
-)
-USING(month)
+-- Code 1:using CTE  
+with
+product_view as(
+  SELECT
+    format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
+    count(product.productSKU) as num_product_view
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+  , UNNEST(hits) AS hits
+  , UNNEST(hits.product) as product
+  WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
+  AND hits.eCommerceAction.action_type = '2'
+  GROUP BY 1
+),
 
-ORDER BY month; 
+add_to_cart as(
+  SELECT
+    format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
+    count(product.productSKU) as num_addtocart
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+  , UNNEST(hits) AS hits
+  , UNNEST(hits.product) as product
+  WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
+  AND hits.eCommerceAction.action_type = '3'
+  GROUP BY 1
+),
+
+purchase as(
+  SELECT
+    format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
+    count(product.productSKU) as num_purchase
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+  , UNNEST(hits) AS hits
+  , UNNEST(hits.product) as product
+  WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
+  AND hits.eCommerceAction.action_type = '6'
+  and product.productRevenue is not null   --phải thêm điều kiện này để đảm bảo có revenue
+  group by 1
+)
+
+select
+    pv.*,
+    num_addtocart,
+    num_purchase,
+    round(num_addtocart*100/num_product_view,2) as add_to_cart_rate,
+    round(num_purchase*100/num_product_view,2) as purchase_rate
+from product_view pv
+left join add_to_cart a on pv.month = a.month
+left join purchase p on pv.month = p.month
+order by pv.month;
+
+-- Code 2: using count(case when) hoặc sum(case when)
+with product_data as(
+select
+    format_date('%Y%m', parse_date('%Y%m%d',date)) as month,
+    count(CASE WHEN eCommerceAction.action_type = '2' THEN product.v2ProductName END) as num_product_view,
+    count(CASE WHEN eCommerceAction.action_type = '3' THEN product.v2ProductName END) as num_add_to_cart,
+    count(CASE WHEN eCommerceAction.action_type = '6' and product.productRevenue is not null THEN product.v2ProductName END) as num_purchase
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+,UNNEST(hits) as hits
+,UNNEST (hits.product) as product
+where _table_suffix between '20170101' and '20170331'
+and eCommerceAction.action_type in ('2','3','6')
+group by month
+order by month
+)
+
+select
+    *,
+    round(num_add_to_cart/num_product_view * 100, 2) as add_to_cart_rate,
+    round(num_purchase/num_product_view * 100, 2) as purchase_rate
+from product_data;
+
 
 -- Calculate cohort map from product view to addtocart to purchase in Jan, Feb and March 2017. 
 -- For example, 100% product view then 40% add_to_cart and 10% purchase.
